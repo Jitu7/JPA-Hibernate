a4ac206801abbce3bb0cc44d61c5f435ccb4f885
Jyotirmaya Das <jyotirmaya12@gmail.com>
2020-03-22 21:00:09 +0530

    Entities and context

---
+++ jpa-e1/src/main/java/com/jyotirmayadas/entities/Product.java
@@ -0,0 +1,53 @@
+package com.jyotirmayadas.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    private int id;
+
+    private String name;
+    private Double price;
+
+    @Column(name = "expiration_date")
+    private LocalDate expirationDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+}
